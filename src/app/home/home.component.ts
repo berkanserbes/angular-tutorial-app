ee12049d9fb71bc0ae66df761c2e924ddcc1d81c
Berkan Serbes <berkan_serbes@hotmail.com>
2023-08-06 10:44:21 +0300

    Modified home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Housinglocation } from '../housing-location/housinglocation';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
+  housingLocation: Housinglocation = {
+    id: 999,
+    name: "Test Home",
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  }
 }
